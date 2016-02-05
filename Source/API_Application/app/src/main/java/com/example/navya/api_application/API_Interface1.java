079fb8c0f1b93f88f465fa825f925567a3b85003
Tejaswiganne <tejuuganne@gmail.com>
2016-02-05 11:35:14 +0530

    Added Screenshots

--- Source/API_Application/app/src/main/java/com/example/navya/api_application/API_Interface1.java
+++ Source/API_Application/app/src/main/java/com/example/navya/api_application/API_Interface1.java
@@ -1,6 +1,7 @@
 package com.example.navya.api_application;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
@@ -28,29 +29,33 @@ public class API_Interface1 extends FragmentActivity  {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_api__interface1);
-        setUpMapIfNeeded();
+     //   setUpMapIfNeeded();
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
       /*  SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);*/
     }
 
-    @Override
+  /*  @Override
     protected void onResume() {
         super.onResume();
         setUpMapIfNeeded();
-    }
+    }*/
 
     public void onSearch(View view)
     {
+        Intent intent = new Intent("com.example.navya.api_application.Locate_Map");
+        startActivity(intent);
         EditText location_present=(EditText)findViewById(R.id.address);
         String location=location_present.getText().toString();
+
         List<Address> addressList=null;
         if(location!=null || !location.equals(""))
         {
             Geocoder geocoder=new Geocoder(this);
             try {
                  addressList= geocoder.getFromLocationName(location, 1);
+
 
             } catch (IOException e) {
                 e.printStackTrace();
@@ -60,20 +65,13 @@ public class API_Interface1 extends FragmentActivity  {
             LatLng latLng=new LatLng(address.getLatitude(),address.getLongitude());
             mMap.addMarker(new MarkerOptions().position(latLng).title("Marker"));
             mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+
         }
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
 
-    private void setUpMapIfNeeded() {
+
+  /*  private void setUpMapIfNeeded() {
         if (mMap == null) {
             mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
             if (mMap != null) {
@@ -82,7 +80,7 @@ public class API_Interface1 extends FragmentActivity  {
         }
     }
 
-    private void setUpMap() {
+   /* private void setUpMap() {
         mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             // TODO: Consider calling
---
+++ Source/API_Application/app/src/main/java/com/example/navya/api_application/Locate_Map.java
@@ -0,0 +1,90 @@
+package com.example.navya.api_application;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
+
+public class Locate_Map extends FragmentActivity  {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_locate__map);
+        setUpMapIfNeeded();
+
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+      //  mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setUpMapIfNeeded();
+    }
+
+    public void onSearch(View view)
+    {
+        EditText location_present=(EditText)findViewById(R.id.address);
+        String location=location_present.getText().toString();
+        List<Address> addressList=null;
+        if(location!=null || !location.equals(""))
+        {
+            Geocoder geocoder=new Geocoder(this);
+            try {
+                addressList= geocoder.getFromLocationName(location, 1);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            Address address=addressList.get(0);
+            LatLng latLng=new LatLng(address.getLatitude(),address.getLongitude());
+            mMap.addMarker(new MarkerOptions().position(latLng).title("Marker"));
+            mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+
+        }
+    }
+
+
+    private void setUpMapIfNeeded() {
+        if (mMap == null) {
+            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
+            if (mMap != null) {
+                setUpMap();
+            }
+        }
+    }
+
+    private void setUpMap() {
+        mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
+    }
+
+
+
+}
